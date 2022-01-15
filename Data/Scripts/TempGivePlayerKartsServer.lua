b4207e8c65d2ce0b823b8aa2a7fe02326b5038e7
TaoOfChaos <addict3d_2cha0s@hotmail.com>
2022-01-15 08:20:28 -0800

    Update TempGivePlayerKartsServer.lua
    * Fixed test scripts to also update Battle menu the same as Karts menu

--- Data/Scripts/TempGivePlayerKartsServer.lua
+++ Data/Scripts/TempGivePlayerKartsServer.lua
@@ -18,22 +18,39 @@ function OnPlayerJoined(player)
     local totalBattleXp = player.serverUserData.totalBattleXp
 
     -- fourth time player tests, they get reset
-    if totalBattleXp == 150000 then
+    if totalBattleXp == 150001 then
         totalBattleXp = nil
         player.serverUserData.totalBattleXp = totalBattleXp
         player.serverUserData.trucks = nil
         player.serverUserData.selectedTruck = nil
     end
 
+    if totalBattleXp == 150000 then
+        totalBattleXp = 150001
+        player.serverUserData.totalBattleXp = totalBattleXp
+    end
+
     -- third time player tests, they'll get 150000 (all vehicles and upgrades should be unlocked)
-    if totalBattleXp == 5000 then
+    if totalBattleXp == 5001 then
         totalBattleXp = 150000
         player.serverUserData.totalBattleXp = totalBattleXp
     end
 
-    -- second time player tests, they'll get 5000 (second vehicle should be unlocked, all it's upgrades locked)
-    if totalBattleXp == 2000 then
+
+    if totalBattleXp == 5000 then
+        totalBattleXp = 5001
+        player.serverUserData.totalBattleXp = totalBattleXp
+    end
+
+    -- third test second vehicle should be unlocked, all it's upgrades locked, and they get 5000
+    if totalBattleXp == 2001 then
         totalBattleXp = 5000
+        player.serverUserData.totalBattleXp = totalBattleXp
+    end
+
+    -- second time player tests, first vehicle and two of it's upgrades should be unlocked
+    if totalBattleXp == 2000 then
+        totalBattleXp = 2001
         player.serverUserData.totalBattleXp = totalBattleXp
     end
     
@@ -70,23 +87,25 @@ function OnPlayerJoined(player)
         player.serverUserData.totalRaceXp = totalRaceXp
     end
 
+
     if totalRaceXp == 5000 then
         totalRaceXp = 5001
         player.serverUserData.totalRaceXp = totalRaceXp
     end
 
+    -- third test second vehicle should be unlocked, all it's upgrades locked, and they get 5000
     if totalRaceXp == 2001 then
         totalRaceXp = 5000
         player.serverUserData.totalRaceXp = totalRaceXp
     end
 
-    -- second time player tests, they'll get 5000 (second vehicle should be unlocked, all it's upgrades locked)
+    -- second time player tests, first vehicle and two of it's upgrades should be unlocked
     if totalRaceXp == 2000 then
         totalRaceXp = 2001
         player.serverUserData.totalRaceXp = totalRaceXp
     end
 
-    -- first time player tests, they'll get 2000 (first vehicle and two of it's upgrades should be unlocked)
+    -- first time player tests, they should see first vehicle only, and they'll get 2000
     if not totalRaceXp or totalRaceXp == 0 then
         player.serverUserData.totalRaceXp = 2000
     end
