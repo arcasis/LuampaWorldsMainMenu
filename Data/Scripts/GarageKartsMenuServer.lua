8cb9c94a66f4e37835821cc9413e20107288cdd0
TaoOfChaos <addict3d_2cha0s@hotmail.com>
2022-01-26 10:17:17 -0800

    Fixed SelectedVehicle bug
    Battle menu was using old selectedVehicle scripts on server side, after receiving broadcast that player chose a vehicle.

--- Data/Scripts/GarageKartsMenuServer.lua
+++ Data/Scripts/GarageKartsMenuServer.lua
@@ -3,27 +3,20 @@
 -- NOTE: When adding new vehicle types, don't forget to include them in LuampaMenuPlayerStorageServer
 -- to save their selection in storage
 
---!! WIP !! Needs redone with empty upgrades table if player is using default kart for selected vehicle
--- Save player's selectedKart, so it can be uploaded to storage when they leave
+-- client sets itself, then broadcasts so server can update
 function OnDefaultKartSelected(player, index)
-    local kartsTable = player.serverUserData.karts     -- get player's karts table
-    local selectedKartUpgrades = kartsTable[index]     -- copy the upgrades for currently viewed vehicle
-    local selectedKartTable = {}     -- create a new, empty table
-    selectedKartTable[index] = selectedKartUpgrades     -- copy the upgrades into the correct index of the empty table
-    player.serverUserData.selectedKart = selectedKartTable     -- save only those upgrades into player's selectedKart data
-
-    -- NOTE: no need to push to client, selectedKart is set also on client side
+    local karts = {}
+    karts[index] = {}
+    player.serverUserData.selectedTruck = karts
 end
 
+-- client sets itself, then broadcasts so server can update
 function OnDefaultKartUpgradeSelected(player, index, upgradeIndex)
-    local kartsTable = player.serverUserData.karts     -- get player's karts table
-    local selectedKartTable = {}     -- create a new, empty karts table
-    selectedKartTable[index] = {}     -- create an new empty kartModel sub-table
-    selectedKartTable[index][upgradeIndex] = kartsTable[index][upgradeIndex]    -- copy upgradeIndex into new table
+    local karts = {}
+    karts[index] = {}
+    karts[index][upgradeIndex] = 1
 
-    player.serverUserData.selectedKart = selectedKartTable     -- save only the one upgrade into player's selectedKart data
-
-    -- NOTE: no need to push to client, selectedKartUpgrade is set also on client side
+    player.serverUserData.selectedKart = karts
 end
 
 Events.ConnectForPlayer("SelectDefaultKart", OnDefaultKartSelected)
--- Data/Scripts/GarageTrucksMenuServer.lua
+++ Data/Scripts/GarageTrucksMenuServer.lua
@@ -3,8 +3,22 @@
 -- NOTE: When adding new vehicle types, don't forget to include them in LuampaMenuPlayerStorageServer
 -- to save their selection in storage
 
+-- client sets itself, then broadcasts so server can update
 function OnDefaultTruckSelected(player, index)
-    player.serverUserData.selectedTruck = index
+
+    local trucks = {}
+    trucks[index] = {}
+    player.serverUserData.selectedTruck = trucks
+end
+
+-- client sets itself, then broadcasts so server can update
+function OnDefaultTruckUpgradeSelected(player, index, upgradeIndex)
+    local trucks = {}
+    trucks[index] = {}
+    trucks[index][upgradeIndex] = 1
+
+    player.serverUserData.selectedTruck = trucks
 end
 
 Events.ConnectForPlayer("SelectDefaultTruck", OnDefaultTruckSelected)
+Events.ConnectForPlayer("SelectDefaultTruckUpgrade", OnDefaultTruckUpgradeSelected)
