faae74517e8fe1543448a70238203a3dc26889a0
TaoOfChaos <addict3d_2cha0s@hotmail.com>
2022-07-08 17:19:42 -0700

    Test prints for helmet purchase
    To see if perks purchases is working to update helmet menu

--- Data/Scripts/LuampaPurchaseHelmetsServer.lua
+++ Data/Scripts/LuampaPurchaseHelmetsServer.lua
@@ -12,16 +12,22 @@ function PerkChanged(player, perk)
 
     if perk == "Speed Duck Helmet" then
         helmets[2] = 1
+        print("Helmet 2 purchased")
     elseif perk == "Serve Please Helmet" then
         helmets[3] = 1
+        print("Helmet 3 purchased")
     elseif perk == "A lick end Helmet" then
         helmets[4] = 1
+        print("Helmet 4 purchased")
     elseif perk == "S4DF4RM04 Helmet" then
         helmets[5] = 1
+        print("Helmet 5 purchased")
     elseif perk == "On control Helmet" then
         helmets[6] = 1
+        print("Helmet 6 purchased")
     elseif perk == "MrD4B00M Helmet" then
         helmets[7] = 1
+        print("Helmet 7 purchased")
     end
 
     player.serverUserData.helmets = helmets
